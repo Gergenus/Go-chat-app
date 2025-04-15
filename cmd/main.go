1aea4449195829db540a05b0d6221658839bc0dc
Dimitr <khakimoff.dima@mail.ru>
2025-04-15 10:01:10 +0300

    Refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,52 +1,19 @@
 package main
 
 import (
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/Gergenus/StandardLib/internal/handler"
-	"github.com/Gergenus/StandardLib/internal/middleware"
-	"github.com/Gergenus/StandardLib/internal/repository"
-	"github.com/Gergenus/StandardLib/internal/service"
-	"github.com/Gergenus/StandardLib/internal/ws"
+	"github.com/Gergenus/StandardLib/internal/server"
 	"github.com/Gergenus/StandardLib/pkg"
-	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	e := echo.New()
-	s := http.Server{
-		Addr:         ":8080",
-		Handler:      e,
-		WriteTimeout: 30 * time.Second,
-		ReadTimeout:  60 * time.Second,
-	}
-
 	pkg.Load()
 	db := pkg.InitDB(os.Getenv("USER"), os.Getenv("PASSWORD"), os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DBNAME"), os.Getenv("SSLMODE"))
 
-	hash := pkg.NewSHAhash(os.Getenv("SALT"))
-	jwtPKG := pkg.NewJWTgo()
-	userRepo := repository.PostgresUserRepo{DB: db, Hash: &hash}
-	authService := service.JWTauth{UserRepo: &userRepo, Hasher: &hash, Auther: &jwtPKG}
-	handlerAuth := handler.NewEchoHandlerAuth(&authService)
-	middle := middleware.NewEchoMiddleware(&jwtPKG)
+	Server := server.NewEchoServer(db)
+	Server.InitializationRouts()
 
-	hub := ws.NewHub()
-	hubHandler := ws.NewHandler(hub)
-
-	e.POST("/SignUp", handlerAuth.SignUp)
-	e.POST("/SignIn", handlerAuth.SignIn)
-	e.GET("/", handler.Access, middle.AuthMiddleware)
-	e.POST("/ws/createRoom", hubHandler.CreateRoom, middle.AuthMiddleware)
-	e.GET("/ws/joinRoom/:roomId", hubHandler.JoinRoom, middle.WSAuthMiddleware)
-
-	e.GET("ws/getRooms", hubHandler.GetRooms, middle.AuthMiddleware)
-	e.GET("/ws/getClients", hubHandler.GetClients, middle.AuthMiddleware) // roomId = 1
-
-	go hub.Run()
-
-	s.ListenAndServe()
+	Server.Start()
 
 }
---
+++ internal/server/server.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Gergenus/StandardLib/internal/handler"
+	"github.com/Gergenus/StandardLib/internal/middleware"
+	"github.com/Gergenus/StandardLib/internal/repository"
+	"github.com/Gergenus/StandardLib/internal/service"
+	"github.com/Gergenus/StandardLib/internal/ws"
+	"github.com/Gergenus/StandardLib/pkg"
+	"github.com/labstack/echo/v4"
+)
+
+type Server interface {
+	Start()
+	InitializationRouts()
+}
+
+type EchoServer struct {
+	App *echo.Echo
+	DB  pkg.DBwraper
+}
+
+func NewEchoServer(DB pkg.DBwraper) EchoServer {
+	App := echo.New()
+	return EchoServer{
+		App: App,
+		DB:  DB,
+	}
+}
+
+func (e *EchoServer) Start() {
+	s := http.Server{
+		Addr:         os.Getenv("HTTPPORT"),
+		Handler:      e.App,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  60 * time.Second,
+	}
+	s.ListenAndServe()
+}
+
+func (e *EchoServer) InitializationRouts() {
+
+	hash := pkg.NewSHAhash(os.Getenv("SALT"))
+	jwtPKG := pkg.NewJWTgo()
+	userRepo := repository.PostgresUserRepo{DB: e.DB, Hash: &hash}
+	authService := service.JWTauth{UserRepo: &userRepo, Hasher: &hash, Auther: &jwtPKG}
+	handlerAuth := handler.NewEchoHandlerAuth(&authService)
+	middle := middleware.NewEchoMiddleware(&jwtPKG)
+
+	hub := ws.NewHub()
+	hubHandler := ws.NewHandler(hub)
+
+	e.App.POST("/SignUp", handlerAuth.SignUp) // {"username": "Denis", "email": "123", "password": "123"}
+	e.App.POST("/SignIn", handlerAuth.SignIn) // {"username": "Denis", "email": "123", "password": "123"}
+	e.App.GET("/", handler.Access, middle.AuthMiddleware)
+	e.App.POST("/ws/createRoom", hubHandler.CreateRoom, middle.AuthMiddleware) // { "id": "1", "name": "chatting"}
+	e.App.GET("/ws/joinRoom/:roomId", hubHandler.JoinRoom, middle.WSAuthMiddleware)
+
+	e.App.GET("ws/getRooms", hubHandler.GetRooms, middle.AuthMiddleware)
+	e.App.GET("/ws/getClients", hubHandler.GetClients, middle.AuthMiddleware)
+
+	go hub.Run()
+}
